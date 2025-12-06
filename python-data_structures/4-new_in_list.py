db2e509409ad55c7a234740680d843a7d21317d4
layan <layansaleem@gmail.com>
2025-12-06 20:15:21 +0000

    4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    # Create a copy of the list
+    new_list = my_list.copy()
+    
+    # Check if idx is valid
+    if idx < 0 or idx >= len(new_list):
+        return new_list
+    
+    # Replace element at the given index in the copy
+    new_list[idx] = element
+    return new_list
