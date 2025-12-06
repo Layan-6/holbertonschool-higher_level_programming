e37f3cff2d0b0ea539fdd0436308575b54a90702
layan <layansaleem@gmail.com>
2025-12-06 20:19:48 +0000

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 def new_in_list(my_list, idx, element):
     # Create a copy of the list
     new_list = my_list.copy()
-    
+
     # Check if idx is valid
     if idx < 0 or idx >= len(new_list):
         return new_list
-    
+
     # Replace element at the given index
     new_list[idx] = element
     return new_list
