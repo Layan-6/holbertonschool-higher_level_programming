a4d2c491dba48439c8b4760ea33ab4c232861a57
layan <layansaleem@gmail.com>
2025-12-06 20:46:34 +0000

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Check if idx is valid
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    
-    # Create a new list without the element at idx
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-    return new_list
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+    return my_list
