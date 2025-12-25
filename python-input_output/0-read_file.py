966455c60d2a194bbf6a102333090b570a9d940e
layan <layansaleem@gmail.com>
2025-12-25 14:24:30 +0000

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ Contains function to read and print text file
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout
-    
+
     Args:
         filename (str): The name of the file to read
     """
