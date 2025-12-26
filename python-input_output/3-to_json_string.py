d9ccfa4680064ab1ec32f6a9fc0b5be10733159e
layan <layansaleem@gmail.com>
2025-12-26 10:46:03 +0000

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -9,10 +9,10 @@ import json
 def to_json_string(my_obj):
     """
     Returns the JSON representation of an object (string)
-    
+
     Args:
         my_obj: The object to convert to JSON
-    
+
     Returns:
         str: JSON string representation of the object
     """
