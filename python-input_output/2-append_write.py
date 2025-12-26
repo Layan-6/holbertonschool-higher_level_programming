ea04a9ef07b097facfae007f9870f76f819ec7e3
layan <layansaleem@gmail.com>
2025-12-26 10:39:56 +0000

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,13 +7,13 @@ Contains function to append string to file and return character count
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file (UTF8) and 
-    returns the number of characters added
-    
+    Appends a string at the end of a text file (UTF8) and and
+returns the number of characters added
+
     Args:
         filename (str): The name of the file to append to
         text (str): The text to append to the file
-    
+
     Returns:
         int: Number of characters added
     """
