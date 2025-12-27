d735fd3d7c941890bf788d0b916a6b658eb19705
layan <layansaleem@gmail.com>
2025-12-27 15:44:14 +0000

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Module: 11-student
+Defines a Student class with serialization and deserialization
+"""
+
+
+class Student:
+    """
+    Student class that defines a student with JSON serialization
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance
+
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): Student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): List of attribute names to include (optional)
+
+        Returns:
+            dict: Dictionary representation of the student
+        """
+        # Get all attributes of the object
+        obj_dict = self.__dict__
+
+        # If attrs is provided and is a list of strings
+        if isinstance(attrs, list) and all(isinstance(item, str)
+                                           for item in attrs):
+            # Filter dictionary to include only specified attributes
+            result = {}
+            for key, value in obj_dict.items():
+                if key in attrs and isinstance(value, (list, dict, str,
+                                                       int, bool)):
+                    result[key] = value
+            return result
+        else:
+            # Return all serializable attributes
+            result = {}
+            for key, value in obj_dict.items():
+                if isinstance(value, (list, dict, str, int, bool)):
+                    result[key] = value
+            return result
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+
+        Args:
+            json (dict): Dictionary with attribute names and values
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
