3d4a4a176776880777c0174b9fddb27d9a81d169
layan <layansaleem@gmail.com>
2025-12-27 15:35:22 +0000

    9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,37 +6,33 @@ Defines a Student class with to_json method
 
 
 class Student:
-"""
-Student class that defines a student
-"""
+    """
+    Student class that defines a student
+    """
 
-def __init__(self, first_name, last_name, age):
-"""
-Initializes a Student instance
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance
 
-Args:
-first_name (str): Student's first name
-last_name (str): Student's last name
-age (int): Student's age
-"""
-self.first_name = first_name
-self.last_name = last_name
-self.age = age
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): Student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self):
-"""
-Retrieves a dictionary representation of a Student instance
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
 
-Returns:
-dict: Dictionary representation of the student
-"""
-# Get all attributes of the object
-obj_dict = self.__dict__
-
-# Filter only serializable types
-result = {}
-for key, value in obj_dict.items():
-if isinstance(value, (list, dict, str, int, bool)):
-result[key] = value
-
-return result
+        Returns:
+            dict: Dictionary representation of the student
+        """
+        obj_dict = self.__dict__
+        result = {}
+        for key, value in obj_dict.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                result[key] = value
+        return result
