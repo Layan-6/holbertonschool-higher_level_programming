e30d8a6a17d20bf127acf386e8f970b454422d6d
layan <layansaleem@gmail.com>
2026-01-03 13:42:07 +0000

    6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-word = "Holberton"
-# ...
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
