0283d465c57139824619be0e824607832c376353
layan <layansaleem@gmail.com>
2026-01-17 19:17:45 +0000

    4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Creates a table called first_table in the current database
+-- Table structure: id INT, name VARCHAR(256)
+-- If table already exists, script should not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
