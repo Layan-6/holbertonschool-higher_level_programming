02dfb070ae6ecc065ee32f027cea8511be60c622
layan <layansaleem@gmail.com>
2026-01-17 19:34:18 +0000

    10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of second_table ordered by score (top first)
+-- Results display score then name
+-- Database name is passed as argument to mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
