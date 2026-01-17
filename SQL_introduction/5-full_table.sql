e46bc134893f3d5177f18d5df8118c076f5f1c59
layan <layansaleem@gmail.com>
2026-01-17 19:20:28 +0000

    5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Prints the full description of the table first_table
+-- Database name is passed as argument to mysql command
+-- Not using DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table;
