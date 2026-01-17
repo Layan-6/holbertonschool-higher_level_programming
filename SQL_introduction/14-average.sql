8a7449a8fdef271978097088b153611486b53674
layan <layansaleem@gmail.com>
2026-01-17 19:50:12 +0000

    14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the score average of all records in second_table
+-- Result column name should be average
+-- Database name is passed as argument to mysql command
+SELECT AVG(score) AS average FROM second_table;
