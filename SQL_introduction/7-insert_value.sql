4e0d052c1815e9cb9c145ca2a66303a511542e5b
layan <layansaleem@gmail.com>
2026-01-17 19:25:57 +0000

    7

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Inserts a new row in the table first_table
+-- New row: id = 89, name = Best School
+-- Database name is passed as argument to mysql command
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
