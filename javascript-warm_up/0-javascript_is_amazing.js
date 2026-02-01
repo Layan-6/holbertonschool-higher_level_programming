23b1616080d96654a93a97cc0055a34b92195050
layan <layansaleem@gmail.com>
2026-02-01 19:09:57 +0000

    0

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-// Task 0: First constant, first print
-// Create a constant variable called myVar with the value "JavaScript is amazing"
-
 const myVar = 'JavaScript is amazing';
-
-// Use console.log to print the output
 console.log(myVar);
